2ca35b31502a706a64cf6a1d64474e1db06fe384
Addison Beck <github@addisonbeck.com>
2024-10-26 16:02:26 -0400

    Skip intro

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -562,6 +562,7 @@ class TitleState extends MusicBeatState
 			switch (sickBeats)
 			{
 				case 1:
+					skipIntro();
 					//FlxG.sound.music.stop();
 					FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 					FlxG.sound.music.fadeIn(4, 0, 0.7);
